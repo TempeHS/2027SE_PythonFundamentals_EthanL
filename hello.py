ed265ae9c55b86e3e2ca13b60389153ff5f59f76
Ethan L <dang.le523@education.nsw.gov.au>
2026-02-15 23:15:35 +0000

    upload completed playback.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-name = input("Whats your name? ")
-print("Hello " + name)
+# ask user for their name
+name = input("What is you name? ")
+
+# Remove whitespace and title for str
+name = name.strip().title()
+
+# say hello to user
+print(f"hello {name}")
---
+++ playback/playback.py
@@ -0,0 +1,4 @@
+user_input = input("Enter more than 2 words ")
+words = user_input.split()
+result = "***".join(words)
+print(result)
