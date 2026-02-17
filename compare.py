d6f89a80db83c1a429c45d858b8d3cfbfbae2353
Ethan L <dang.le523@education.nsw.gov.au>
2026-02-17 03:04:24 +0000

    upload completed deep.p

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+if x < y:
+    print("X is less than y ")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x and y are equal")
---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+user_input = input("Answer the GREAT QUESTION OF LIFE! ")
+correct_respond = ["42", "fourty_two", "fourty two"]
+if user_input in correct_respond:
+    print("yes")
+else:
+    print("no")
