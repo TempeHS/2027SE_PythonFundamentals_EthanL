46e8cb53cb99b7e31ee52bfaa60a521eb7542630
Ethan L <dang.le523@education.nsw.gov.au>
2026-02-19 02:35:21 +0000

    Upload completed bank.py

---
+++ bank/bank.py
@@ -0,0 +1,7 @@
+user_input = input("Greet me ")
+if "hello" in user_input.lower():
+    print("$0")
+elif "How you doing?" in user_input:
+    print("$20")
+elif "What's happening" in user_input:
+    print("$100")
