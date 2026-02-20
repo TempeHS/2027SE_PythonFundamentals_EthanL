d5ae54b451c53c4c1ded11bbe31fac0b598acf2d
Ethan L <dang.le523@education.nsw.gov.au>
2026-02-20 00:15:35 +0000

    complete

--- extensions/interpreter/interpreter.py
+++ extensions/interpreter/interpreter.py
@@ -4,4 +4,10 @@ x = float(x)
 z = float(z)
 if y == "+":
     result = x + z
+elif y == "-":
+    result = x - z
+elif y == "x":
+    result = x * z
+elif y == "/":
+    result = x / z
 print(result)
