c1636e92caeae3c6711c0e3c1341d8eca35c25dd
Ethan L <dang.le523@education.nsw.gov.au>
2026-03-22 18:25:34 +0000

    upload complete test-twttr

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
-x = int(input("What's x? "))
-y = int(input("What's y> "))
-print(x + y)
+def main():
+    x = int(input("What's x?"))
+    print("x squared is ", square(x))
+
+
+def square(n):
+    return n * n
+
+
+if __name__ == "__name__":
+    main()
